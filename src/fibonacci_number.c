783754fab151ace604d551d22e65c9d40227fdb4
Inndy <Inndy.mail@gmail.com>
2014-05-24 10:34:59 +0800

    Add tail recursion version

--- src/fibonacci_number.c
+++ src/fibonacci_number.c
@@ -1,18 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef unsigned long long int ullint;
 
 long long int memo[512];
 long long int fib(int);
-int main(){
+ullint fib_r(int n, ullint a, ullint b);
+
+int main() {
 
 	int n;
-	for (n = 1; n <= 90; n++){
+	for (n = 1; n <= 90; n++) {
 		printf("result of fib %d is :%lld\n", n,fib(n));
 	}
 	
 	
-	
+	printf("\nfib_r now!\n\n");
+	for (n = 1; n <= 90; n++) {
+		printf("result of fib_r(%d) is %lld\n", n, fib_r(n, 1, 1));
+	}
 
 	
 	return 0;
@@ -31,4 +37,11 @@ long long int fib(int n){
 	}
 	memo[n] = temp_result;
 	return temp_result;
+}
+
+ullint fib_r(int n, ullint a, ullint b) {
+	if (n > 1)
+		return fib_r(n - 1, a + b, a);
+	else
+		return b;
 }
